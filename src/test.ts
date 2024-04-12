02913bc51a603520a5cb0decb2d8b1051c4cc5da
Svetlana Vladi <lanavladi05@gmail.com>
2024-04-12 23:08:27 +0500

    add: set up webpack

---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export function someFunc(param: number): string {
+    console.log('RANDON FUNCTION');
+    return '';
+}
---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+
+const config: webpack.Configuration = {
+    mode: 'development',
+    // mode: 'production',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    // entry: './src/index.js',
+    output: {
+        // filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new webpack.ProgressPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+
+};
+export default config;
